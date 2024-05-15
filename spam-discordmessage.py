5ada56847299becc55fbb9c2f7f91ec6ad43f54e
Scopped <126022433+Code-Dev23@users.noreply.github.com>
2024-05-15 20:02:20 +0200

    Add files via upload

---
+++ spam-discordmessage.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+import subprocess
+
+time.sleep(2)
+#subprocess.Popen(r"C:\Users\coder\AppData\Local\Discord\app-1.0.9146\Discord.exe")
+#time.sleep(5)
+#pyautogui.click(x=500, y=700) 
+
+while True:
+    pyautogui.write("Stupida bambina!")
+    pyautogui.press('enter')
+
+    #time.sleep(10)
